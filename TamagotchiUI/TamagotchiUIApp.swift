63d51e0877fcdd77aa696f9085c1e688b6e02c56
Leo Hammond <hammond.l@etoncollege.org.uk>
2023-01-14 10:50:02 +0000

    Initial Commit

---
+++ TamagotchiUI/TamagotchiUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamagotchiUIApp.swift
+//  TamagotchiUI
+//
+//  Created by Hammond, Leo (NA) on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct TamagotchiUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
